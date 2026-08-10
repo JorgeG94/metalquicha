aa05ada3201518718dc640d181dfdb2d803905e0
Jorge Luis Galvez Vallejo <jorgegalvez1694@gmail.com>
2026-08-10 23:05:15 +1000

    clean

--- env.sh
+++
@@ -1,3 +0,0 @@
-module load gcc/15.1.0
-module load mkl
-module load openmpi/5.0.5
--- nvhpc_env.sh
+++
@@ -1,7 +0,0 @@
-#module load comm_libs/openmpi/nvhpc-5.0.5
-export VERNO=25.5
-module load nvhpc
-module load misc/nvhpc-build/${VERNO}/netcdf-c/
-module load misc/nvhpc-build/${VERNO}/netcdf-fortran/
-module load hdf5
-export PYTHON=python3
