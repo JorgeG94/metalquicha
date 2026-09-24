2d0558d9599d4c2cf7bf971575c73e884670826c
JorgeG94 <JorgeG94@users.noreply.github.com>
2026-09-24 03:23:59 +0000

    deploy: b744a27d343b968618e3563b829a60341be59849

--- src/mqc_version.f90
+++ src/mqc_version.f90
@@ -1,6 +1,7 @@
 !! Version information for metalquicha
 module mqc_version
    use pic_logger, only: logger => global_logger
+   use mqc_build_info, only: build_info_line
    implicit none
    private
 
@@ -44,6 +45,10 @@ contains
                        " ecp="//available(ecp_backend_available())// &
                        " dftd3="//available(dispersion_available())// &
                        " dftd4="//available(dispersion_d4_available()))
+      ! How it was built, on the line after what it can do. A timing is only
+      ! reproducible alongside this: the BLAS choice alone moves a density
+      ! functional run by a factor of five and cannot be seen from the outside.
+      call logger%info(build_info_line())
    end subroutine print_version
 
    pure function available(is_available) result(text)
